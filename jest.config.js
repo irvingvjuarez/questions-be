ffc48dd39247bb1796f2830cf336e6fabbc8fc24
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-31 11:26:38 -0600

    jest config added

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const config = {
+	verbose: true,
+	// transformIgnorePatterns: [
+	// 	'node_modules/(?!' +
+	// 	[
+	// 		'node-fetch',
+	// 		'fetch-blob',
+	// 		'data-uri-to-buffer',
+	// 		'jest-runtime',
+	// 		'formdata-polyfill'
+	// 	].join('|') +
+	// 	')',
+	// ],
+};
+
+module.exports = config;
